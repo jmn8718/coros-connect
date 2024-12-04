14082cf1248f42482fd9df6d3831e590a6c900c9
Jose Miguel Navarro <jmn8718@gmail.com>
2024-12-04 11:57:23 +0900

    update example and vscode settings

--- example/index.ts
+++ example/index.ts
@@ -22,22 +22,24 @@ async function run() {
   });
   const activity = activitiesData.dataList[0];
 
-  // get activity details by activity Id
-  // same data as it is fetched from getActivitiesList
-  const activityData = await coros.getActivityDetails(activity.labelId);
+  if (activitiesData.dataList) {
+    // get activity details by activity Id
+    // same data as it is fetched from getActivitiesList
+    const activityData = await coros.getActivityDetails(activity.labelId);
+    
+    // get file url to download
+    const fileUrl = await coros.getActivityDownloadFile({
+      activityId: activity.labelId,
+      fileType: 'fit',
+    });
+    // example of file url https://s3.coros.com/fit/XXX/YYY.fit
   
-  // get file url to download
-  const fileUrl = await coros.getActivityDownloadFile({
-    activityId: activity.labelId,
-    fileType: 'fit',
-  });
-  // example of file url https://s3.coros.com/fit/XXX/YYY.fit
-
-  // download file to disk, you can use any other way you want
-  await downloadFile({
-    fileUrl,
-    filePath: path.join('.', `_${activity.labelId}.fit`),
-  });
+    // download file to disk, you can use any other way you want
+    await downloadFile({
+      fileUrl,
+      filePath: path.join('.', `_${activity.labelId}.fit`),
+    });
+  }
 }
 
 run();
